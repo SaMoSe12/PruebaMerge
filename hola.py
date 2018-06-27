3ffd452bc7829b2972e31cb129837ca08586035d
santiago@fertifarma.com <santiago@fertifarma.com>
2018-06-27 16:48:45 -0500

    Se modifico hola.py

--- hola.py
+++ hola.py
@@ -1,2 +1,13 @@
 print("hola!")
 
+
+"""
+
+Este es un comentario 
+de mas de una linea
+pense que te podria interesar...
+
+"""
+
+print("Los comentarios con \"\"\" Son mejores que los cochinos comentarios con #!")
+
